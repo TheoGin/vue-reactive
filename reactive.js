cb480d11feb6e4eda40041efc33a4c08f24b12c7
Sophotheo <2559629448@qq.com>
2025-04-12 17:02:41 +0800

    fix: reactive think

--- reactive.js
+++ reactive.js
@@ -24,13 +24,13 @@ export function reactive(target) {
         get(target, key){
             // 依赖收集
             track(target, key);
-            return target[key];
+            return target[key]; // 返回对象的相应属性值
         },
         set(target, key, value){
             // 派发更新
             trigger(target, key);
             // proxy里面的set需要返回一个boolean值，表示 赋值成功true | 赋值失败 ---> 
-            return Reflect.set(target, key, value); // 相当于target[key] = value，但反射赋值会返回一个boolean值
+            return Reflect.set(target, key, value); // 设置对象的相应属性值。相当于target[key] = value，但反射赋值会返回一个boolean值
         },
     })
 }
